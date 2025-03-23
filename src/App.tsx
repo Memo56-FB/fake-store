a1938834dbcf3028d6c437dbd3693888bb9fad37
Memo56-FB <memo56_fb@hotmail.com>
2025-03-23 12:21:14 -0600

    🎨Feat: add initial UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Layout } from "./Layout"
+
 function App() {
 
   return (
-    <>
-     <h1 className='grid place-items-center text-4xl font-bold'>Hello SPS</h1>
-    </>
+    <Layout>
+      <h1 className='grid place-items-center text-4xl font-bold'>Hello SPS</h1>
+    </Layout>
   )
 }
 
