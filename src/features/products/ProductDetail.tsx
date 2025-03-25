bf5a59f2886404f40a4f3da0ef0a4f4dfb2ebf79
Memo56-FB <memo56_fb@hotmail.com>
2025-03-24 20:21:08 -0600

    💻Feat(productDetail): add responsive UI

--- src/features/products/ProductDetail.tsx
+++ src/features/products/ProductDetail.tsx
@@ -3,10 +3,13 @@ import useProductDetail from "./hooks/useProductDetail"
 import { renderRating } from "./utils/renderRating"
 import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
-import { Loader2 } from "lucide-react"
+import { House, Loader2, Store } from "lucide-react"
+import { useMediaQuery } from "usehooks-ts"
+import { Separator } from "@radix-ui/react-separator"
 
 export const ProductDetail = () => {
   const { data, isLoading } = useProductDetail()
+  const isDesktop = useMediaQuery("(min-width: 1024px)")
 
   if (isLoading) {
     return (
@@ -20,10 +23,12 @@ export const ProductDetail = () => {
 
   return (
     <section className="p-4">
-      <Card className="gap-2">
-        <CardHeader className="text-muted-foreground text-sm font-bold">
-          {data?.title}
-        </CardHeader>
+      <Card className="gap-2 lg:grid lg:grid-cols-3">
+        {!isDesktop &&
+          <CardHeader className="text-muted-foreground text-sm font-bold md:text-2xl md:text-black">
+            {data?.title}
+          </CardHeader>
+        }
         <CardContent>
           <article className="flex items-center gap-3">
             <div className="flex">
@@ -33,18 +38,44 @@ export const ProductDetail = () => {
           </article>
           <img src={data?.image} alt={data?.title} className="w-full h-full object-cover" />
         </CardContent>
-        <CardDescription className="p-3">
-          {data?.description}
-        </CardDescription>
-        <CardFooter>
-          <p className="text-xl font-bold flex">
-            <span className="text-red-600 text-xs">$</span>
-            {data?.price.toString().split('.')[0]}
-            <span className="text-xs">{data?.price.toString().split('.')[1]}</span>
-          </p>
-          <p className="text-sm"></p>
-        </CardFooter>
+        <div>
+          {isDesktop &&
+            <CardHeader className="text-3xl font-bold p-0">
+              {data?.title}
+            </CardHeader>
+          }
+          {isDesktop &&
+            <Separator className="w-full h-0.5 bg-red-600 my-4" />
+
+          }
+          <CardDescription className="p-3 lg:p-0">
+            {data?.description}
+          </CardDescription>
+          {isDesktop &&
+            <Separator className="w-full h-0.5 bg-red-600 my-4" />
+          }
+          <CardFooter>
+            <p className="text-xl md:text-4xl font-bold flex">
+              <span className="text-red-600 text-xs md:text-base">$</span>
+              {data?.price.toString().split('.')[0]}
+              <span className="text-xs md:text-base">{data?.price.toString().split('.')[1]}</span>
+            </p>
+            <p className="text-sm"></p>
+          </CardFooter>
+        </div>
         <div className="p-3">
+          {isDesktop &&
+            <div>
+              <p className="p-6 rounded-md bg-gray-100 flex gap-2 mb-12">
+                Selecciona una tienda para conocer si puedes recoger este producto el día de hoy
+                <Store className="text-red-600" />
+              </p>
+              <p className="p-6 rounded-md bg-gray-100 flex gap-2 mb-12">
+                Recibelo a domicilio <House className="text-red-600" />
+              </p>
+
+            </div>
+          }
           <Button className="bg-yellow-300 text-black rounded-xl w-full">
             Agregar al carrito
           </Button>
