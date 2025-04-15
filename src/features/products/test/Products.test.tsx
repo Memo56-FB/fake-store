59131f6999f5b7371c809b6019b74e5673c2cacd
Memo56-FB <memo56_fb@hotmail.com>
2025-04-14 21:05:05 -0600

    🧪 Test: change product test

--- src/features/products/test/Products.test.tsx
+++ src/features/products/test/Products.test.tsx
@@ -1,11 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Home } from "@/features/products/Home";
 import '@testing-library/jest-dom'
-import { beforeEach, describe, expect, it } from "vitest";
+import { describe, expect, it, Mock, vi } from "vitest";
 import { MemoryRouter } from "react-router";
+import { getAllProducts } from "../productsServices";
 
 const queryClient = new QueryClient()
+
+vi.mock('../productsServices', () => ({
+  getAllProducts: vi.fn()
+}))
+
+const mockGetAllProducts = getAllProducts as Mock
+
+const mockProuducts = [
+  { id: 1, title: 'Producto 2', category: 'Electrónica', description: 'Descripción del producto 1', price: 100, rating: { rate: 4.5, count: 100} }
+]
 
 const renderHome = () =>
   render(
@@ -18,18 +29,14 @@ const renderHome = () =>
 
 
 describe('<Home />', () => {
-  beforeEach(renderHome)
-
-  
-  it('should render a loader when the products are loading', () => {
-    expect(screen.getAllByText('Loading')).toHaveLength(5)
-  })
-
-
   it('should render products', async () => {
-    const loaders = screen.getAllByText('Loading')
-    await waitForElementToBeRemoved(loaders)
-    const message = await screen.getByText('Producto 1')
-    expect(message).toBeInTheDocument()
+    mockGetAllProducts.mockResolvedValue(mockProuducts)
+    renderHome()
+    expect(screen.getAllByText('Loading')).toHaveLength(5)
+    await waitFor(() => {
+      const product = screen.getByText('Producto 2')
+      expect(product).toBeInTheDocument()
+    })
   })
+
 })
